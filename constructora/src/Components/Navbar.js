392942127ca98dbe77722ef99d3271ab17c42871
Atomlejandro <atomlejandro@gmail.com>
2023-03-28 16:41:20 -0500

    Barra de navegación y header agregados

--- constructora/src/Components/Navbar.js
+++ constructora/src/Components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../css/Navbar.css';
 import logo from '../images/papasito.jpeg';
-
+import 'bootstrap/dist/css/bootstrap.css';
 const Navbar = () => {
     return (
-        <div>
-            <nav>
+        <div class='navegacion bg-dark'>
+            {/* <nav>
                 <img src={logo} alt="Logo" className="logo"/>
                 <ul>
                     <li><a href="#">Inicio</a></li>
@@ -14,7 +14,28 @@ const Navbar = () => {
                     <li><a href="#">Contacto</a></li>
                     <li><a href="#">Proyectos</a></li>
                 </ul>
-            </nav>
+            </nav> */}
+
+            <div class="container bg-dark">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom ">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+       
+        <img src={logo} alt="Logo" className="logo"/>
+        <span class="fs-4 text-white">Simple header</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Inicio</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">¿Quiénes somos?</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">Proyectos</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">Servicios</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">Contacto</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">Proyectos</a></li>
+       
+       
+      </ul>
+    </header>
+  </div>
         </div>
     );
 }
